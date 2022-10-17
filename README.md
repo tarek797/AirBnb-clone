5246e97d73b71df88ee7f1f61a70ca7834d54ffe
Tarek Habash <tarek.soliman797@gmail.com>
2022-10-18 00:22:30 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# AirBnb clone
+
+## making use of React custom components and declarative capabilities of react to create Airbnb clone with a few lines of code 
+
+Quick start:
+
+```
+$ yarn # npm install
+$ yarn build # npm run build
+````
+
+## Development
+
+Run Webpack in watch-mode to continually compile the JavaScript as you work:
+
+```
+$ yarn watch # npm run watch
+```
+## Screenshots
+
+
+![](images/screen-shot.png)
+
+## Authors
+
+- [@Tarek Habash](https://github.com/tarek797)
---
+++ data.js
@@ -0,0 +1,80 @@
+export default [
+    {
+        id: 1,
+        title: "Cooking experience hosted by Johann",
+        description: "Apple Strudel baking lesson & Lunch",
+        price: 47,
+        coverImg: "strudel-baking.png",
+        stats: {
+            rating: 4.94,
+            reviewCount: 194
+        },
+        location: "Salzburg,Austria",
+        openSpots: 13,
+    },
+    {
+        id: 2,
+        title: "Learn Wedding Photography",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 125,
+        coverImg: "wedding-photography.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Töpfern an der Drehscheibe mit einer Keramikerin",
+        description: "Experience hosted by Carmen",
+        price: 65,
+        coverImg: "pottery.png",
+        stats: {
+            rating: 4,
+            reviewCount: 2
+        },
+        location: "Niederndorf bei Kufstein, Austria",
+        openSpots: 4,
+    },
+    {
+        id: 4,
+        title: "Tasting of Austrian products",
+        description: "Experience hosted by Rainer",
+        price: 30,
+        coverImg: "Rainer.png",
+        stats: {
+            rating: 4.9,
+            reviewCount: 171
+        },
+        location: "Vienna,Austria",
+        openSpots: 30,
+    },
+    {
+        id: 5,
+        title: "Life Lessons with Katie Zaferes",
+        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        price: 136,
+        coverImg: "katie-zaferes.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 6,
+        title: "Group Mountain Biking",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "mountain-bike.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Norway",
+        openSpots: 3,
+    }
+]
